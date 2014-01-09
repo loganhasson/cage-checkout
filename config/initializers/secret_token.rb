a5beec0978279b5e16915daa86e77207648cf9fb
Logan Hasson <logan.hasson@gmail.com>
2014-01-09 13:56:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CageCheckout::Application.config.secret_key_base = '797ecff9484eec6962318af858600f1f2352f4f1721cbdf08417c32f2bebe772dede9e0924e893556fc26bb6338ddaf04443176006004b686aaa28454fac4d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CageCheckout::Application.config.session_store :cookie_store, key: '_cage-checkout_session'
